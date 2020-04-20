b8263ed88a3812f27f636494ad1315e8f12659ea
Daniel Augusto <danguto@gmail.com>
2020-04-20 02:17:20 +0100

    cesar exercicio 4 complete

--- CesaExercicio4/app/src/main/java/com/daniel/cesaexercicio4/MainActivity.java
+++ CesaExercicio4/app/src/main/java/com/daniel/cesaexercicio4/MainActivity.java
@@ -2,7 +2,10 @@ package com.daniel.cesaexercicio4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -11,6 +14,8 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private ViewHolder mViewHolder = new ViewHolder();
+    ArrayAdapter<String> adapter;
+    String[] database = new String[]{"bake", "find", "list", "patterns", "found", "misspelling"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,18 +25,113 @@ public class MainActivity extends AppCompatActivity {
         this.mViewHolder.seach = findViewById(R.id.search);
         this.mViewHolder.list = findViewById(R.id.list);
 
-        String[] date = new String[] {"bake", "find", "list", "patterns", "found", "misspelling"};
-
-        ArrayAdapter<String> adapter;
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, date);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, this.database);
 
         this.mViewHolder.list.setAdapter(adapter);
 
+        this.mViewHolder.seach.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String toSearch = mispelling(s.toString());
+                (MainActivity.this).adapter.getFilter().filter(toSearch);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
     }
 
+    //ViewHolder
     private static class ViewHolder {
         EditText seach;
         ListView list;
+    }
+
+    //check Jubled
+    public boolean checkJubled(String orig, String comp) {
+        String original = orig;
+        String compare = comp;
+        Boolean work = true;
+        int count = 0;
+
+        if (original.charAt(0) == compare.charAt(0)) {
+            if (original.length() > 3) {
+                for (int i = 1; i < original.length() && work; i++) {
+                    if (original.charAt(i) != compare.charAt(i))
+                        count++;
+                    if (count >= original.length() * 2 / 3)
+                        work = false;
+                }
+            }
+        } else
+            work = false;
+
+        return work;
+    }
+
+    //check jubled
+    public boolean checkTypo(String ori, String comp) {
+        String original = ori;
+        String compare = comp;
+        boolean work = true;
+
+        //check replaced
+        if (original.length() == compare.length()) {
+            int count = 0;
+            for (int i = 0; i < original.length() && work; i++) {
+                //check replaced
+                if (original.charAt(i) != compare.charAt(i)) {
+                    count++;
+                }
+                if (count >= 2)
+                    work = false;
+            }
+        } else
+            //check added
+            if (original.length() < compare.length())
+                if (compare.length() - original.length() == 1)
+                    for (int i = 0; i < compare.length() - 1 && work; i++) {
+                        if (compare.charAt(i) != original.charAt(i))
+                            if (original.charAt(i) != compare.charAt(i + 1))
+                                work = false;
+                    }
+                else
+                    work = false;
+            else
+                //check removed
+                if (original.length() > compare.length())
+                    if (original.length() - compare.length() == 1)
+                        for (int i = 0; i < original.length() - 1 && work; i++) {
+                            if (compare.charAt(i) != original.charAt(i))
+                                if (original.charAt(i + 1) != compare.charAt(i))
+                                    work = false;
+                        }
+                    else
+                        work = false;
+
+        return work;
+    }
+
+    //correct misspelling
+    public String mispelling(String in) {
+        for (int i = 0; i < this.database.length; i++) {
+            if (checkTypo(this.database[i], in))
+                return this.database[i];
+            else if (this.database[i].length() == in.length()) {
+                if (checkJubled(this.database[i], in)) {
+                    return this.database[i];
+                }
+            }
+        }
+        return in;
     }
 
 
